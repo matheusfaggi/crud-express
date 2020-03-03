cf3edb513e1ea663fd86843a338bcbb4117503ee
Matheus Faggi <matheus.faggi@br-mediagroup.com>
2020-03-03 18:02:00 -0300

    refactoring routes

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
+const routes = require("./src/routes");
 const PORT = 3000;
 const server = express();
 
-server.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
-});
+server.use(routes);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
